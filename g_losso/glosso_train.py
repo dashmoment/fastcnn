19b976fb76fdeb45b4d92e88d2c62d00fae9c87a
Jesse <dashmoment1017@gmail.com>
2017-04-25 17:58:55 +0800

    g losso train

--- g_losso/glosso_train.py
+++ g_losso/glosso_train.py
@@ -7,27 +7,6 @@ import model_utility as mu
 import yolo_netfactory as nf
 import YOLO_tiny_tf
 
-batch_file = "/media/ubuntu/65db2e03-ffde-4f3d-8f33-55d73836211a/dataset/VOC_train"
-test_file = "/media/ubuntu/65db2e03-ffde-4f3d-8f33-55d73836211a/dataset/VOC_val"
-filename = "../../model/glosso/fcann_v1.ckpt"
-logfile = '../../log/glosso'
-graph_model = '../../model/glosso/fcann_v1.ckpt-0.meta'
-checkpoint_dir = '../../model/glosso'
-
-train_type = "RMS"
-continue_training = 0
-loop_num = 0
-batch_size = 64
-save_epoch = 10
-test_epoch = 10
-
-
-yolo = YOLO_tiny_tf.YOLO_TF()
-keep_prob = tf.placeholder(tf.float32)
-x = tf.placeholder(tf.float32,(None,448,448,3))
-label = tf.placeholder(tf.float32,(None,1470))
-tlabel = tf.placeholder(tf.float32,(None,1470))
-
 
 def recursive_create_var(scope, Nlayers, reduce_percent, init_layers):
     
@@ -43,7 +22,7 @@ def recursive_create_var(scope, Nlayers, reduce_percent, init_layers):
 
                 shape = init_layers[i][1]
                 
-                if idx != 0: 
+                if idx >= 0: 
                                        
                     if i > 0 and len(shape)  > 1 : shape[-2] = int(init_layers[i-1][1][-1])
                     
@@ -52,7 +31,7 @@ def recursive_create_var(scope, Nlayers, reduce_percent, init_layers):
                 
                 if init_layers[i][0] == 'fc12w':  init_layers[i][1][-1] = 1470
                 if init_layers[i][0] == 'fc12b':  init_layers[i][1][-1] = 1470          
-                if init_layers[i][0] == 'fc10w' and idx == 1:
+                if init_layers[i][0] == 'fc10w' and idx == 0:
                     init_layers[i][1][-2] = 40131
                    
           
@@ -67,6 +46,28 @@ def recursive_create_var(scope, Nlayers, reduce_percent, init_layers):
 def get_graph_var():
 
     [print(n.name) for n in tf.get_default_graph().as_graph_def().node]
+
+
+batch_file = "/media/ubuntu/65db2e03-ffde-4f3d-8f33-55d73836211a/dataset/VOC_train"
+test_file = "/media/ubuntu/65db2e03-ffde-4f3d-8f33-55d73836211a/dataset/VOC_val"
+filename = "../../model/glosso/fcann_v1.ckpt"
+logfile = '../../log/glosso'
+graph_model = '../../model/glosso/fcann_v1.ckpt-0.meta'
+checkpoint_dir = '../../model/glosso'
+
+train_type = "RMS"
+continue_training = 1
+loop_num = 4500
+batch_size = 64
+save_epoch = 200
+test_epoch = 500
+
+
+yolo = YOLO_tiny_tf.YOLO_TF()
+keep_prob = tf.placeholder(tf.float32)
+x = tf.placeholder(tf.float32,(None,448,448,3))
+label = tf.placeholder(tf.float32,(None,1470))
+tlabel = tf.placeholder(tf.float32,(None,1470))
     
 
 model_ticket = {'root':'yolo_tiny', 'branch':'vanilla'}
@@ -74,59 +75,39 @@ init_layers = mu.model_zoo(model_ticket)
 var_dict = recursive_create_var('recursive', 1, 0.2, init_layers)
 var_list = var_dict['recursive_0']
 
-var = {}
-sum_stack = []
-  
-with tf.variable_scope('recursive_0') as scope:
-    scope.reuse_variables()
-    for index in range(0,len(var_list),2):
-        var[var_list[index]] = tf.get_variable(var_list[index])
-        var[var_list[index+1]] = tf.get_variable(var_list[index+1])
-        
-        if index < 18:
-            wsum = tf.reduce_sum(tf.sqrt(tf.reduce_sum(tf.square(var[var_list[index]]), axis=[0,1,2])))
-            bsum = tf.reduce_sum(tf.sqrt(tf.reduce_sum(tf.square(var[var_list[index+1]]))))
-            
-        else:
-            wsum = tf.reduce_sum(tf.sqrt(tf.reduce_sum(tf.square(var[var_list[index]]), axis=0)))
-            bsum = tf.reduce_sum(tf.sqrt(tf.reduce_sum(tf.square(var[var_list[index+1]]))))
-           
-        sum_stack.append(wsum)
-        sum_stack.append(bsum)
-            
-        
-    glosso = tf.reduce_sum(tf.stack(sum_stack,0))        
-
+with tf.name_scope('Weight_sum'):
+    with tf.variable_scope('recursive_0') as scope:
+        scope.reuse_variables()
+        g_losso = tf.add_n([tf.reduce_sum(tf.norm(tf.reshape(tf.get_variable(x),[-1,tf.get_variable(x).get_shape().as_list()[-1]]),axis=0)) for x in var_list])
+        [tf.summary.histogram(x, tf.get_variable(x), collections=['test']) for x in var_list]
 
 lossTicket = {'loss':'gL2norm'}
 glosso_train = nf.glosso_train("recursive_0", 'train', x, var_dict, keep_prob, True)
-
-loss = {'prob':glosso_train, 'gloss':glosso}
-gloss = mu.loss_zoo(lossTicket, loss, label)
-L2_Solver = tf.train.AdamOptimizer(1e-4).minimize(gloss[0])
-
-
-#tlossTicket = {'loss':'gL2norm'}
-#glosso_test = nf.glosso_train("recursive_0", 'test', x, var_dict, keep_prob, False)
-#tloss = {'prob':glosso_test, 'gloss':glosso}
-#tgloss = mu.loss_zoo(tlossTicket, tloss, tlabel)  
+loss_pair = {'prob':glosso_train, 'gloss':g_losso}
+loss = mu.loss_zoo(lossTicket, loss_pair, label)
+L2_Solver = tf.train.AdamOptimizer(1e-4).minimize(loss[0])
 
 
-tf.summary.scalar('Train_RMSE',gloss[0],collections=['train'])
-#tf.summary.scalar('Train_Glossp',gloss[1],collections=['train'])
-#tf.summary.scalar('Test_RMSE',tgloss[0],collections=['test'])
-#tf.summary.scalar('Test_Glosso',tgloss[1],collections=['test'])
+tlossTicket = {'loss':'gL2norm'}
+glosso_test = nf.glosso_train("recursive_0", 'test', x, var_dict, keep_prob, False)
+tloss_pair = {'prob':glosso_test, 'gloss':g_losso}
+tloss = mu.loss_zoo(tlossTicket, tloss_pair, tlabel)  
+
+
+tf.summary.scalar('Train_RMSE',loss[0],collections=['train'])
+tf.summary.scalar('Train_Glossp',loss[1],collections=['train'])
+tf.summary.scalar('Test_RMSE',tloss[0],collections=['test'])
+tf.summary.scalar('Test_Glosso',tloss[1],collections=['test'])
 
 merged_summary_train = tf.summary.merge_all('train')
-#merged_summary_test= tf.summary.merge_all('test')
+merged_summary_test= tf.summary.merge_all('test')
 
 
 
 with tf.Session() as sess:
     
     summary_writer = tf.summary.FileWriter(logfile, sess.graph)  
-    summary = {'writer':summary_writer, 'train':merged_summary_train}
-    #summary = {'writer':summary_writer, 'train':merged_summary_train, 'test':merged_summary_test} 
+    summary = {'writer':summary_writer, 'train':merged_summary_train, 'test':merged_summary_test} 
     saver = tf.train.Saver()    
     
     if continue_training !=0:
@@ -139,7 +120,6 @@ with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())  
         
     
-    
     for i in range(loop_num, 1000000):
        
        print("Epoch:{}".format(i))
@@ -149,14 +129,16 @@ with tf.Session() as sess:
        feeddict = {x:data_labels['data'], label:data_labels['label'], keep_prob:0.5}
  
        if i%save_epoch == 0: savemodel = True
-       mu.train_op(sess, train_type, L2_Solver, gloss[0], feeddict, savemodel, saver, filename, summary, i)
+       mu.train_op(sess, train_type, L2_Solver, loss[0], feeddict, savemodel, saver, filename, summary, i)
       
-#       if i%test_epoch == 0:#
+       if i%test_epoch == 0:#
 
-#           tdata_labels = mu.gnerate_dl_pairs(yolo, test_file, batch_size, (448,448,3))
-#           tfeeddict = {x:tdata_labels['data'], tlabel:tdata_labels['label'], keep_prob:0.5}
-#           mu.test_op(sess, train_type, tgloss[0], tfeeddict, summary, i)
+           tdata_labels = mu.gnerate_dl_pairs(yolo, test_file, batch_size, (448,448,3))
+           tfeeddict = {x:tdata_labels['data'], tlabel:tdata_labels['label'], keep_prob:0.5}
+           mu.test_op(sess, train_type, tloss[0], tfeeddict, summary, i)
 
+    summary_writer.close()
+    sess.close()  
     
 #get_graph_var()
 
