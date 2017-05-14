6b65014b264b634b5ec655159dd8611f2c1802a6
dashmoment <dashmoment1017@gmail.com>
2017-05-14 17:25:44 +0800

    weight pruning individual dim

--- yolo/dirtytest.py
+++ yolo/dirtytest.py
@@ -13,57 +13,6 @@ import time
 import model_utility as mu
 import matplotlib.pyplot as plt
 
-
-def weight_pruning(yolo_obj, sess, key_pairs, var_list):
-    
-    for var in var_list:
-        tensors = yolo_old.sess.run(tf.get_default_graph().get_tensor_by_name(key_pairs[var]+':0'))
-        
-        with tf.variable_scope('recursive_1') as scope:
-            scope.reuse_variables() 
-            tensors_s =  sess.run(tf.get_variable(var))
-              
-        if np.shape(tensors) != np.shape(tensors_s):
-                        
-                if (len(np.shape(tensors))) > 1:
-                
-                    if (len(np.shape(tensors))) > 2:                         
-                        dim_sum = np.sum(np.sum(np.sum(tensors, axis=0),axis=0), axis=1)
-                    else:
-                        dim_sum = np.sum(tensors, axis=1)
-                    
-                    dim_sum = np.abs(dim_sum)
-                    dim_sort = np.unravel_index(dim_sum.argsort(axis=None), dims=int(len(dim_sum)))
-                    del_list = [dim_sort[0][x] for x in range(np.shape(tensors)[-2] -  np.shape(tensors_s)[-2])]
-                    
-                    dim_array = np.delete(tensors,del_list, -2)
-                    
-                    if (len(np.shape(tensors))) > 2:        
-                        kernel_sum = np.sum(np.sum(np.sum(dim_array,axis=0), axis=0),axis=0)
-                    else:
-                        kernel_sum = np.sum(dim_array, axis=0)
-                    
-                    kernel_sum = np.abs(kernel_sum)
-                    kernel_sort = np.unravel_index(kernel_sum.argsort(axis=None), dims=int(len(kernel_sum)))
-                    kdel_list = [kernel_sort[0][x] for x in range(np.shape(tensors)[-1] -  np.shape(tensors_s)[-1])]
-                    kernel_array = np.delete(dim_array, kdel_list, -1)
-                
-                else:
-                    kernel_array = np.delete(tensors, kdel_list,0)
-                
-        else:
-            kernel_array = tensors
-
-    
-        with tf.variable_scope('recursive_1') as scope:
-                scope.reuse_variables() 
-                op = tf.assign(tf.get_variable(var), kernel_array)
-                sess.run(op)
-                tensors_s =  sess.run(tf.get_variable(var))
-            
-            
-    return kernel_array
-
 key_pairs = {
             'conv1w':'Variable',
             'conv1b':'Variable_1',
@@ -92,6 +41,8 @@ key_pairs = {
             
             }
 
+
+
 def recursive_create_var(scope, Nlayers, reduce_percent, init_layers):
     
     scope_dict = {}
@@ -102,6 +53,7 @@ def recursive_create_var(scope, Nlayers, reduce_percent, init_layers):
         with tf.variable_scope(scope_name):
             
             name_dict = []
+            
             for i in range(len(init_layers)):
 
                 shape = init_layers[i][1]
@@ -118,22 +70,26 @@ def recursive_create_var(scope, Nlayers, reduce_percent, init_layers):
                 if init_layers[i][0] == 'fc12w':  init_layers[i][1][-1] = 1470
                 if init_layers[i][0] == 'fc12b':  init_layers[i][1][-1] = 1470          
                 if init_layers[i][0] == 'fc10w':
+                
                     if idx == 0:
                         init_layers[i][1][-2] = 50176
                     if idx == 1:
-                        init_layers[i][1][-2] = 45129
+                        init_layers[i][1][-2] = 47628
+                    if idx == 2:
+                        init_layers[i][1][-2] = 32095
                    
           
                 tf.get_variable(init_layers[i][0],init_layers[i][1], initializer=tf.contrib.layers.xavier_initializer())
 
                 name_dict.append(init_layers[i][0])
+                
         scope_dict[scope_name] = name_dict 
 
     return scope_dict
 
 
 img_root = '/media/ubuntu/65db2e03-ffde-4f3d-8f33-55d73836211a/dataset/VOCdevkit/VOC2007/Test/JPEGImages'
-shrink_ratio = 0.9
+shrink_ratio = 0.95
 
 if os.path.isdir(img_root):
 
@@ -150,12 +106,13 @@ with tf.device('/gpu:0'):
     
     model_ticket={'root':'yolo_tiny', 'branch':'vanilla'}
     init_layers = mu.model_zoo(model_ticket)
-    var_dict = recursive_create_var('recursive', 2, shrink_ratio, init_layers)
-    var_list = var_dict['recursive_0']
+    var_dict = recursive_create_var('recursive', 3, shrink_ratio, init_layers)
     
 x = tf.placeholder(tf.float32,(None,448,448,3))
 label = tf.placeholder(tf.float32,(None,1470), name='labels')
 keep_prob = tf.placeholder(tf.float32)
+
+var_scope = 'recursive_1'
 
 config = tf.ConfigProto()
 config.gpu_options.allow_growth=True
@@ -163,14 +120,18 @@ with tf.Session(config = config) as sess:
     
      sess.run(tf.global_variables_initializer())  
      
-     res = weight_pruning(yolo_old, sess, key_pairs, var_list)
+     
+#     mu.weight_pruning_ind(yolo_old, sess, var_dict[var_scope], var_scope)
+     
+     
+     mu.weight_pruning(yolo_old, sess, var_dict[var_scope], var_scope)
 
 
-     fpath = '/home/dashmoment/workspace/dataset/VOCdevkit/VOC2007/Test/JPEGImages/000011.jpg'    
+     fpath = '/home/dashmoment/workspace/dataset/VOCdevkit/VOC2007/Test/JPEGImages/000002.jpg'    
      w,h,inputs = ut.vocimg_preprocess(fpath)
      src = cv2.imread(fpath)
      
-     yolo_ds = nf.glosso_train("recursive_1", 'test', x, var_dict, keep_prob, False)  
+     yolo_ds = nf.glosso_train(var_scope, 'test', x, var_dict, keep_prob, False)  
      
      for i in range(10):
     
@@ -187,23 +148,47 @@ with tf.Session(config = config) as sess:
          print("Time:{}".format(e-s))
      
      
-    
+     label = prob_label_old
+     predict = prob_label
+     label_cls = tf.slice(label, [0,0],[-1, 980])
+     label_offset = tf.slice(label, [0, 980],[-1, 490])
+
+     pre_cls = tf.slice(predict, [0,0],[-1, 980])
+     pre_offset = tf.slice(predict, [0, 980],[-1, 490])
+
+    #===========KL Divergence of each class====================
+     label_cls = tf.nn.softmax(tf.reshape(label_cls, [-1,49,20] ))
+     pre_cls = tf.nn.softmax(tf.reshape(pre_cls, [-1,49,20]))
+     kl_loss = tf.reduce_sum(tf.multiply(pre_cls, tf.log(tf.divide(pre_cls,label_cls))), axis=[1,2])
+
+    #============SmoothL1 for rest parts======================
+     res_value = tf.abs(tf.subtract(pre_offset,label_offset))
+     smoothL1 = tf.cast(tf.less(res_value,1), tf.float32)
+     invsmoothL1 = tf.cast(tf.less(smoothL1,0.5),tf.float32)
+     r1 = tf.multiply(tf.square(res_value), smoothL1)*0.5
+     r2 = tf.multiply((res_value - 0.5), invsmoothL1)       
+     offset_loss = tf.reduce_sum(tf.add(r1,r2), axis=1)#
      
+     skl_loss = sess.run(kl_loss)
+     soffset_loss = sess.run(offset_loss)
+     loss = sess.run(tf.add(kl_loss, offset_loss))
      
-     
-#     lossTicket = {'loss':'yolo_kl_l1'}
-#     loss_pair = {'prob':prob_label, 'gloss':0}  
-#     loss = mu.loss_zoo(lossTicket, loss_pair, label)
-#     
-#     cost = sess.run(loss, feed_dict={x:inputs, label:prob_label_old, keep_prob:1})
-#     
-#     
+     l_cls = sess.run(label_cls)
+     p_cls = sess.run(pre_cls)
+
 #     results_old = ut.interpret_output(prob_label_old[0],w,h)
 #     results = ut.interpret_output(prob_label[0],w,h)
 #     
 #     ut.show_results(src,results_old)
 #     cv2.waitKey()
 #     ut.show_results(src,results)
+     
+     tp = 0
+     
+     for k in range(49):
+         if np.argmax(l_cls[0,k,:]) == np.argmax(p_cls[0,k,:]):
+             tp =tp + 1
+         
 
 
 
